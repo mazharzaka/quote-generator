a151ff3c8e19af102aed282fd4286f7eef1bc1d2
mazharzaka <mazharmohamed566@gmail.com>
2023-12-18 16:33:10 +0200

    add social-btns

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import './App.css';
-import React, {useState} from 'react';
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  TelegramIcon,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+  XIcon,
+} from "react-share";
+import "./App.css";
+import React, {useState} from "react";
 
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
-
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
+  const [show, setShow] = useState(false);
+  const newurl = window.location.href;
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +45,68 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
-          <button onClick={generateQuote}>Generate Another Quote</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
+          <button onClick={generateQuote} className="gen">
+            Generate Another Quote
+          </button>
+          <button
+            className="share"
+            onClick={() => {
+              setShow(!show);
+            }}>
+            +share
+          </button>
         </div>
       </div>
+      {show && (
+        <div className="social-btns">
+          <div className="item">
+            <LinkedinShareButton
+              url={newurl}
+              title={"quote"}
+              summary={quote.content}
+              source={quote.author}>
+              <LinkedinIcon size={32} round />
+            </LinkedinShareButton>
+          </div>
+          <div className="item">
+            <FacebookShareButton
+              url={newurl}
+              quote={quote.content}
+              hashtag="Quote">
+              <FacebookIcon size={32} round />
+            </FacebookShareButton>
+          </div>
+          <div className="item">
+            <TwitterShareButton
+              url={newurl}
+              quote={quote.content}
+              hashtag="Quote">
+              <XIcon size={32} round />
+            </TwitterShareButton>
+          </div>
+          <div className="item">
+            <WhatsappShareButton
+              url={newurl}
+              quote={quote.content}
+              hashtag="Quote">
+              <WhatsappIcon size={32} round />
+            </WhatsappShareButton>
+          </div>
+          <div className="item">
+            <TelegramShareButton
+              url={newurl}
+              quote={quote.content}
+              hashtag="Quote">
+              <TelegramIcon size={32} round />
+            </TelegramShareButton>
+          </div>
+        </div>
+      )}
     </>
-  )
-}
-
+  );
+};
 
 export default App;
